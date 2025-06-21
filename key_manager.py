729560fdfc56dca97d9eddfa78530192d9337318
AI Invigilate <GoodnessThembaC@gmail.com>
2025-06-21 13:44:43 +0200

    create load key and get fernet by key

--- key_manager.py
+++ key_manager.py
@@ -8,3 +8,14 @@ def generate_key():
     with open(KEY_FILE, "wb") as f:
         f.write(key)
 
+def load_key():
+    if not os.path.exists(KEY_FILE):
+        generate_key()
+    with open(KEY_FILE, "rb") as f:
+        return f.read()
+    
+def get_fernet():
+    key = load_key()
+    return Fernet(key=key)
+
+
